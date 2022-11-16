fa068d9444df5085ce3e7f7ca01830b7cf683f28
Tekyto <Tekyto2014@gmail.com>
2022-11-16 09:47:47 +0200

    variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getDistance(distanceTraveled, traveledPercent) {
 	const onePercent = distanceTraveled / traveledPercent;
 	const totalDistance = onePercent * 100;
-	return "distance between village and capital: " + totalDistance + "m";
+	return 'distance between village and capital: ' + totalDistance + 'm';
 }
 
 module.exports = getDistance;
